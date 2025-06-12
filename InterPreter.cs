53ad23ec7ce722fc4f207e2b8dac4353f213c7be
Kubin2001 <jakubmackiewicz5@gmail.com>
2025-06-12 21:43:15 +0200

    Library and Dependency Linking

--- InterPreter.cs
+++ InterPreter.cs
@@ -15,8 +15,10 @@ namespace Methane2._0{
         string configPath = null;
         string vsPath = null;
         bool ready = false;
-        private Dictionary<string, Action<string>> Commands;
+        private Dictionary<string, Action> Commands;
         private string fullLine = null;
+        private string currentCommand = null;
+        private List<string> args = new List<string>();
 
         public InterPreter(string confPath, string vsPath) {
             if(confPath != null && vsPath != null){
@@ -28,30 +30,18 @@ namespace Methane2._0{
         }
 
         private void CreateCommands() {
-            Commands = new Dictionary<string, Action<string>>();
+            Commands = new Dictionary<string, Action>();
             Commands["AddIncludeDir"] = AddIncludeDir;
+            Commands["AddLibraryDir"] = AddLibraryDir;
+            Commands["AddDependencies"] = AddDependencies;
+
         }
 
-        private void AddIncludeDir(string command) {
-
-            bool pathRegister = false;
-            string includePath = "";
-            foreach (char elem in fullLine.ToCharArray()) {
-                if (elem == '(') {
-                    pathRegister = true;
-                    continue;
-                }
-                if (elem == ')') {
-                    break;
-                }
-                if (pathRegister) {
-                    includePath += elem;
-                }
-
+        private void AddIncludeDir() {
+            if(args.Count() != 1) {
+                Console.WriteLine("Error in AddIncludeDir to many args expected 1");
+                return;
             }
-            Console.WriteLine(includePath);
-            includePath += ';';
-
 
             XmlDocument doc = new XmlDocument();
             XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
@@ -64,11 +54,11 @@ namespace Methane2._0{
                 foreach (XmlNode node in clCompileNodes) {
                     XmlNode additionalIncludesNode = node.SelectSingleNode("msb:AdditionalIncludeDirectories", nsmgr);
                     if (additionalIncludesNode != null) {
-                        additionalIncludesNode.InnerText += $";{includePath}";
+                        additionalIncludesNode.InnerText += $";{args[0]}";
                     }
                     else {
                         XmlElement newElement = doc.CreateElement("AdditionalIncludeDirectories", nsmgr.LookupNamespace("msb"));
-                        newElement.InnerText = includePath;
+                        newElement.InnerText = args[0];
                         node.AppendChild(newElement);
                     }
                 }
@@ -79,32 +69,131 @@ namespace Methane2._0{
             }
         }
 
+        private void AddLibraryDir() {
+            if (args.Count() != 1) {
+                Console.WriteLine("Error in AddLibraryDir to many args expected 1");
+                return;
+            }
+
+            XmlDocument doc = new XmlDocument();
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+            nsmgr.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+
+            try {
+                doc.Load(vsPath);
+                XmlNodeList linkNodes = doc.SelectNodes("//msb:Project/msb:ItemDefinitionGroup/msb:Link", nsmgr);
+                foreach (XmlNode node in linkNodes) {
+                    XmlNode additionalLibDirsNode = node.SelectSingleNode("msb:AdditionalLibraryDirectories", nsmgr);
+                    if (additionalLibDirsNode != null) {
+                        additionalLibDirsNode.InnerText += $";{args[0]}";
+                    }
+                    else {
+                        XmlElement newElement = doc.CreateElement("AdditionalLibraryDirectories", nsmgr.LookupNamespace("msb"));
+                        newElement.InnerText = args[0];
+                        node.AppendChild(newElement);
+                    }
+                }
+                doc.Save(vsPath);
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Failed to modify project: " + ex.Message);
+            }
+        }
+
+        private void AddDependencies() {
+            string combinedLibs = "";
+            foreach (string elem in args) { 
+                combinedLibs += elem + ";";
+            }
+
+            XmlDocument doc = new XmlDocument();
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+            nsmgr.AddNamespace("msb", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+
+            try {
+                doc.Load(vsPath);
+
+                XmlNodeList linkNodes = doc.SelectNodes("//msb:Project/msb:ItemDefinitionGroup/msb:Link", nsmgr);
+                foreach (XmlNode node in linkNodes) {
+                    XmlNode additionalDepsNode = node.SelectSingleNode("msb:AdditionalDependencies", nsmgr);
+                    if (additionalDepsNode != null) {
+                        additionalDepsNode.InnerText += $";{combinedLibs}";
+                    }
+                    else {
+                        XmlElement newElement = doc.CreateElement("AdditionalDependencies", nsmgr.LookupNamespace("msb"));
+                        newElement.InnerText = combinedLibs;
+                        node.AppendChild(newElement);
+                    }
+                }
+                doc.Save(vsPath);
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Failed to modify project: " + ex.Message);
+            }
+        }
+
         public void Run(){
             if (ready) {
                 String[] config = File.ReadAllLines(this.configPath);
                 foreach (string line in config) {
                     string parsedLine = "";
+                    string arg = "";
+                    bool collectArg = false;
                     if (line.Contains('(')) {
                         foreach (char elem in line) {
-                            if (elem == '(') {
-                                break;
+                            if (collectArg) {
+                                if (elem == ')') {
+                                    break;
+                                }
+                                if (elem == ',') {
+                                    args.Add(arg);
+                                    arg = "";
+                                    continue;
+                                }
+                                if (elem == ' ') {
+                                    continue;
+                                }
+                                arg += elem;
+
                             }
-                            parsedLine += elem;
+                            else {
+                                if (elem == '(') {
+                                    collectArg = true;
+                                    continue;
+                                }
+                                parsedLine += elem;
+                            }
+                        }
+                        if(arg != "") {
+                            args.Add(arg);
                         }
                     }
                     else { 
                         parsedLine = line;
                     }
+
+                    Console.WriteLine(parsedLine);
+                    foreach (string elem in args) { 
+                        Console.WriteLine(elem);
+                    }
+
+
                     if (Commands.ContainsKey(parsedLine)) {
                         fullLine = line;
-                        Commands[parsedLine](parsedLine);
+                        Commands[parsedLine]();
                     }
                     else {
                         Console.WriteLine("Command Does not exist");
                     }
+
+                    parsedLine = "";
+                    arg = "";
+                    collectArg = false;
+                    args.Clear();
                 }
             }
-
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Methane2.0
 
-Simple win forms app for easy library linking to Visual Studio C++ created only cause i have Cmake
+Simple win forms app for easy library linking to Visual Studio C++ created only cause i hate Cmake
 
 It includes its own interpretet config language
 
